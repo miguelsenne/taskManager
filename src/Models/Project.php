c96a5c7f7b47771af27df461864b2ae9f3cb8192
Miguel Senne <miguelsennejunior@gmail.com>
2019-09-19 06:02:47 -0300

    Using phpsniffer on project.php

--- src/Models/Project.php
+++ src/Models/Project.php
@@ -1,83 +1,82 @@
-<?php  
+<?php
 
 namespace miguelsenne\TaskManager\Models;
+
 use Sinergi\Token\StringGenerator;
 
-class Project {
-	
-	/**
-	 * Armazenamento de projetos
-	 * @var array
-	 */
-	public $projects = [];
+class Project
+{
 
-	/**
-	 * Store an project
-	 * @param  string $name project name
-	 * @return string mensagem de sucesso
-	 */
-	public function store(string $name)
-	{
-		$name = preg_replace("/[0-9\s]+$/", null, $name);
-		
-		return $this->projects[] = [
-			"id" => $this->generateID(),
-			"name" => $name
-		];	
-	}
+    /**
+     * Armazenamento de projetos
+     * @var array
+     */
+    public $projects = [];
 
-	/**
-	 * Generate an ID
-	 * 
-	 * @return string the ID
-	 */
-	public function generateID()
-	{
-		return StringGenerator::randomId();
-	}
+    /**
+     * Store an project
+     * @param  string $name project name
+     * @return string mensagem de sucesso
+     */
+    public function store(string $name)
+    {
+        $name = preg_replace("/[0-9\s]+$/", null, $name);
 
-	/**
-	 * delete an item from projects
-	 * 
-	 * @param string $id the ID
-	 * @return array projects
-	 */
-	public function delete(string $id)
-	{
-		$projects = $this->projects;
+        return $this->projects[] = [
+            "id" => $this->generateID(),
+            "name" => $name
+        ];
+    }
 
-		$key = array_search($id, array_column($projects, 'id'));
+    /**
+     * Generate an ID
+     *
+     * @return string the ID
+     */
+    public function generateID()
+    {
+        return StringGenerator::randomId();
+    }
 
-		unset($this->projects[$key]);
+    /**
+     * delete an item from projects
+     *
+     * @param string $id the ID
+     * @return array projects
+     */
+    public function delete(string $id)
+    {
+        $projects = $this->projects;
 
-		return $this->projects;
-	}
+        $key = array_search($id, array_column($projects, 'id'));
 
-	/**
-	 * Find an project by ID
-	 * 
-	 * @param string $id the ID
-	 * @return array selected project
-	 */
-	public function find(string $id)
-	{
-		$projects = $this->projects;
+        unset($this->projects[$key]);
 
-		$key = array_search($id, array_column($projects, 'id'));
-		
-		return ($key !== false) ? $projects[$key] : [];
-	}
+        return $this->projects;
+    }
 
-	/**
-	 * Reset items in array
-	 * 
-	 * @return array an empty array
-	 */
-	public function reset()
-	{
-		$this->projects = [];
-	}
+    /**
+     * Find an project by ID
+     *
+     * @param string $id the ID
+     * @return array selected project
+     */
+    public function find(string $id)
+    {
+        $projects = $this->projects;
 
+        $key = array_search($id, array_column($projects, 'id'));
+
+        return ($key !== false) ? $projects[$key] : [];
+    }
+
+    /**
+     * Reset items in array
+     *
+     * @return array an empty array
+     */
+    public function reset()
+    {
+        $this->projects = [];
+    }
 }
-
-?>
