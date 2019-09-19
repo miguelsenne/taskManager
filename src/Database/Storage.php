7dd31be9d505c25fd8bbf52d1bf165d000da38d4
Miguel Senne <miguelsennejunior@gmail.com>
2019-09-18 22:22:51 -0300

    adding storage

--- src/Database/Storage.php
+++ src/Database/Storage.php
@@ -1,10 +1,85 @@
 <?php
 
 namespace miguelsenne\TaskManager\Database;
+use Sinergi\Token\StringGenerator;
 
 class Storage 
 {
+    
+    static $data = [];
 
+    /**
+	 * Store an data
+	 * 
+	 * @param string $collection collection name to store your items
+	 * @param array $name data to store
+	 * @return array storaged data
+	 */
+	public static function store(string $collection, array $data)
+	{
+		$data['id'] = self::generateID();
+
+		return self::$data[$collection][] = $data;
+	}
+
+	/**
+	 * delete an item from some collection
+	 * 
+	 * @param string $collection the collection
+	 * @param string $id the ID
+	 * @return array some selected collection
+	 */
+	public static function delete(string $collection, string $id)
+	{
+		if(!array_key_exists($collection, self::$data)) throw new \Exception("Collection not found", 1);
+
+		$items = self::$data[$collection];
+
+		$key = array_search($id, array_column($items, 'id'));
+
+		unset(self::$data[$collection][$key]);
+
+		return self::$data[$collection];
+	}
+
+	/**
+	 * Find an item from some collection
+	 * 
+	 * @param string $collection the collection
+	 * @param string $where the attribute to be search
+	 * @param string $id the value
+	 * @return array selected item
+	 */
+	public static function find(string $collection, string $where, string $value)
+	{
+		if(!array_key_exists($collection, self::$data)) throw new \Exception("Collection not found", 1);
+		
+		$items = self::$data[$collection];
+
+		$key = array_search($value, array_column($items, $where));
+		
+		return ($key !== false) ? self::$data[$collection][$key] : [];
+	}
+
+	/**
+	 * Generate an ID
+	 * 
+	 * @return string the ID
+	 */
+	public static function generateID()
+	{
+		return StringGenerator::randomId();
+	}
+
+	/**
+	 * Reset all data
+	 * 
+	 * @return array an empty array
+	 */
+	public static function reset()
+	{
+		self::$data = [];
+	}
 }
 
 ?>
--- tests/StorageTest.php
+++ tests/StorageTest.php
@@ -4,10 +4,129 @@ namespace miguelsenne\Tests;
 
 use PHPUnit\Framework\TestCase;
 use miguelsenne\TaskManager\Database\Storage;
+use Faker\Factory;
 
 class StorageTest extends TestCase
 {
+
+	public function tearDown(): void
+	{
+		Storage::reset();
+	}
+
+	public function testIfDataIsEmpty()
+	{
+		$this->assertCount(0, Storage::$data);
+	}
+
+	public function testIfItemHasBeenStored()
+	{
+		$item = Storage::store('foo', ['bar' => 'foo']);
+
+		$this->assertEquals('foo', $item['bar']);
+
+		$this->assertCount(1, Storage::$data['foo']);
+	}
 	
+	public function testIfHasBeenStoredMultipleItems()
+	{
+		for($i = 0;$i < 4;$i++) {
+			Storage::store('foo', ['bar' => 'foo']);
+		}
+
+		$this->assertCount(4, Storage::$data['foo']);
+	}
+
+	public function testIfHasBeenNotStoredWithAnyParamters()
+	{
+		$this->expectException("ArgumentCountError");
+
+		Storage::store();
+	}
+
+	public function testIfNotFoundAnCollectionWhenOnDelete()
+	{
+		$this->expectException('Exception');
+
+		$this->expectExceptionMessage('Collection not found');
+
+		Storage::delete('bar', Storage::generateID());
+	}
+
+	public function testIfItemHasBeenDeleted()
+	{
+		for($i = 0; $i < 10;$i++) {
+			$item = Storage::store('foo', ['bar' => 'foo']);
+		}
+		
+		Storage::delete('foo', $item['id']);
+
+		$this->assertCount(9, Storage::$data['foo']);
+	}
+
+	public function testIfHasBeenFounded()
+	{
+		$faker = Factory::create();
+		
+		Storage::store('persons', ['name' => 'Miguel Senne']);
+
+		for($i = 0; $i < 10;$i++){
+			$item = Storage::store('persons', ['name' => $faker->name]);
+		}
+		
+		$search = Storage::find('persons', 'name', 'Miguel Senne');
+
+		$this->assertArraySubset(['name' => 'Miguel Senne'], $search);		
+
+		$find_item = Storage::find('persons', 'name', $item['name']);
+
+		$this->assertArraySubset(['name' => $item['name']], $find_item);
+	}
+
+	public function testSeeAnEmptyArrayWhenNotFound()
+	{
+		Storage::store('persons', ['bar' => 'foo']);
+
+		$search = Storage::find('persons', 'name', 'miguel');
+
+		$this->assertEmpty($search);
+	}
+
+	public function testIfNotFoundAnCollectionWhenOnFind()
+	{
+		$this->expectException('Exception');
+
+		$this->expectExceptionMessage('Collection not found');
+
+		Storage::find('bar', 'name', 'John Doe');
+	}
+
+	public function testIfGenerateIdIsUniq()
+	{
+		for($i = 0; $i < 100;$i++)
+		{
+			$hash[] = Storage::generateID(); 
+		}
+
+		$this->assertEquals(1, array_count_values($hash)[$hash[10]]);
+		$this->assertEquals(1, array_count_values($hash)[$hash[20]]);
+		$this->assertEquals(1, array_count_values($hash)[$hash[5]]);
+	}
+
+	public function testIfDataReturnAnEmptyArrayOnReset()
+	{
+		$faker = Factory::create();
+
+		for($i = 0; $i < 5;$i++){
+			Storage::store('items', ['name' => $faker->name]);
+		}
+
+		Storage::reset();
+		
+		$this->assertIsArray(Storage::$data);
+		$this->assertEmpty(Storage::$data);
+	}
+
 }
 
 ?>
