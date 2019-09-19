4763f158b83e3fcb17824d6dfddce1015e127e32
Miguel Senne <miguelsennejunior@gmail.com>
2019-09-19 06:47:15 -0300

    using phpsniffer on projecttest.php

--- tests/ProjectTest.php
+++ tests/ProjectTest.php
@@ -1,4 +1,4 @@
-<?php  
+<?php
 
 namespace miguelsenne\Tests;
 
@@ -8,84 +8,81 @@ use miguelsenne\TaskManager\Models\Project;
 class ProjectTest extends TestCase
 {
 
-	protected $project;
+    protected $project;
 
-	public function setUp(): void
-	{
-		$this->project = new Project;
-		
-		$this->project->store('Estudos de PHPUnit');
-	}
+    public function setUp(): void
+    {
+        $this->project = new Project;
 
-	public function tearDown(): void
-	{
-		$this->project->reset();
-	}
+        $this->project->store('Estudos de PHPUnit');
+    }
 
-	public function dataProviderAttributtes()
-	{
-		return [
-			["projects"]
-		];
-	}
-	
-	/**
-	 * Test project attributes is empty by default
-	 *
-	 * @dataProvider dataProviderAttributtes
-	 * 
-	 * @return void
-	 */
-	public function testProjectAttributesIsEmptyByDefault($name)
-	{
-		$this->project->reset();
+    public function tearDown(): void
+    {
+        $this->project->reset();
+    }
 
-		$this->assertEmpty($this->project->$name);
-	}
+    public function dataProviderAttributtes()
+    {
+        return [
+            ["projects"]
+        ];
+    }
 
-	public function testProjectStoreItem()
-	{
-		$this->project->reset();
+    /**
+     * Test project attributes is empty by default
+     *
+     * @dataProvider dataProviderAttributtes
+     *
+     * @return void
+     */
+    public function testProjectAttributesIsEmptyByDefault($name)
+    {
+        $this->project->reset();
 
-		$this->project->store('Estudos de PHP');
-		
-		$this->assertCount(1, $this->project->projects);
-	}
+        $this->assertEmpty($this->project->$name);
+    }
 
-	public function testProjectStoreOnlyWords()
-	{
-		$project = $this->project->store('Estudos de PHP 7');
+    public function testProjectStoreItem()
+    {
+        $this->project->reset();
 
-		$this->assertEquals('Estudos de PHP', $project['name']);
-	}
+        $this->project->store('Estudos de PHP');
 
-	public function testProjectStoreGenerateNewIdOnStore()
-	{
-		$project = $this->project->store('Estudos de PHP');
+        $this->assertCount(1, $this->project->projects);
+    }
 
-		$this->assertArrayHasKey('id', $project);
-	}
+    public function testProjectStoreOnlyWords()
+    {
+        $project = $this->project->store('Estudos de PHP 7');
 
-	public function testProjectDelete()
-	{
-		$this->project->reset();
+        $this->assertEquals('Estudos de PHP', $project['name']);
+    }
 
-		$project = $this->project->store('Estudos de PHP');
+    public function testProjectStoreGenerateNewIdOnStore()
+    {
+        $project = $this->project->store('Estudos de PHP');
 
-		$projects = $this->project->delete($project['id']);
+        $this->assertArrayHasKey('id', $project);
+    }
 
-		$this->assertCount(0, $projects);
-	}
+    public function testProjectDelete()
+    {
+        $this->project->reset();
 
-	public function testFindAnProject()
-	{
-		$project = $this->project->store('Lista de tarefas');
-		
-		$project = $this->project->find($project['id']);	
+        $project = $this->project->store('Estudos de PHP');
 
-		$this->assertEquals('Lista de tarefas', $project['name']);
-	}
-	
+        $projects = $this->project->delete($project['id']);
+
+        $this->assertCount(0, $projects);
+    }
+
+    public function testFindAnProject()
+    {
+        $project = $this->project->store('Lista de tarefas');
+
+        $project = $this->project->find($project['id']);
+
+        $this->assertEquals('Lista de tarefas', $project['name']);
+    }
 }
-
-?>
