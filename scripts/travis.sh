d97db68c265f963b8c1c769bd52c757321036480
Miguel Senne <miguelsennejunior@gmail.com>
2019-09-26 00:49:19 -0300

    permision and path absolute

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -4,10 +4,10 @@ cd ${TRAVIS_BUILD_DIR}
 
 if [[ $TRAVIS_PHP_VERSION = '7.2' ]]; then
     phpunit
-    /vendor/bin/phpcs
+    ${pwd}/vendor/bin/phpcs
 fi
 
 if [[ $TRAVIS_PHP_VERSION = '7.3' ]]; then
     phpunit
-    /vendor/bin/phpcs
+    ${pwd}/vendor/bin/phpcs
 fi
